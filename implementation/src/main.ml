e6dc405fe62ef8f9ce9c12c47565844c28e817f8
FranciscoQuinaz <franciscoquinaz@tecnico.ulisboa.pt>
2020-07-13 20:08:34 +0100

    makefile, _tags and main.ml added with a simple argument reader.

---
+++ implementation/src/main.ml
@@ -0,0 +1,32 @@
+print_string "=====================\n\tECMA-SL\n=====================\n"
+
+let file = ref ""
+let mode = ref ""
+let out = ref ""
+let verb_aux = ref false
+
+(* Argument read function *)
+let arguments () =
+	   let usage_msg = "Usage: -i <path> -mode <c/> -o <path> -v <bool> " in
+	   Arg.parse
+	     [
+	        ("-i", Arg.String (fun f -> file := f), "input file")
+			;("-mode", Arg.String (fun m -> mode := m ), "mode to run: c - Core / p - Plus ")
+			;("-o", Arg.String (fun o -> out := o ), "output file")
+			;("-v", Arg.Set verb_aux, "verbose")
+
+	     ]
+	     (fun s -> Printf.printf "Ignored Argument: %s" s)
+	     usage_msg
+
+(* Main function - Run *)
+let run ()=
+
+	arguments();
+	if (!file = "" && !mode = "" && !out = "") then print_string "No option selected. Use -h"
+	else if (!file = "") then  (print_string "No input file. Use -i\n=====================\n\tFINISHED\n=====================\n";exit 1)
+	else if(!mode = "") then  (print_string "No mode selected. Use -mode\n=====================\n\tFINISHED\n=====================\n";exit 1);
+
+	print_string "=====================\n\tFINISHED\n=====================\n"
+
+let _ = run ()
