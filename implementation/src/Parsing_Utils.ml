068921fa9a67ca765b57910bc4bdfb9e31d13a3a
José Santos <j3fsantos@gmail.com>
2020-08-11 18:00:22 +0100

    tail recursion - breaking change

--- implementation/src/Parsing_Utils.ml
+++ implementation/src/Parsing_Utils.ml
@@ -39,3 +39,45 @@ and resolve_import (import : string) : E_Func.t list =
   let e_prog_resolved = resolve_imports e_prog in
   let funcs = Hashtbl.fold (fun fname func acc -> acc @ [func]) e_prog_resolved.funcs [] in
   funcs
+
+module SS = Set.Make(String)
+
+let rec resolve_imports_2 (to_resolve : string list) (resolved : SS.t) (path : string list) : E_Func.t list = 
+  match to_resolve with 
+    | [] -> [] 
+    | file :: files ->
+      if (SS.mem file resolved) 
+        then resolve_imports_2 files resolved path 
+        else (if (List.mem file path)
+                then raise (Error "cyclic dependency")
+                else (
+                  let file_contents = load_file file in
+                  let cur_prog = parse_e_prog file_contents in
+                  let cur_prog_funcs = E_Prog.get_funcs cur_prog in 
+                  let cur_prog_import_funcs = resolve_imports_2 cur_prog.imports resolved (file :: path) in 
+                  let resolved =  SS.add file resolved in 
+                  let rest_files_funcs = resolve_imports_2 files resolved path in 
+                  cur_prog_funcs @ cur_prog_import_funcs @ rest_files_funcs
+                ))
+
+
+let rec resolve_imports_3 (to_resolve : string list list) (resolved : SS.t) (path : string list) (funcs : E_Func.t list) : E_Func.t list = 
+  match (to_resolve, path) with 
+    | ([], _) -> funcs
+    | ([] :: lst, file :: rest_path) ->
+      let resolved =  SS.add file resolved in 
+      resolve_imports_3 lst resolved rest_path funcs 
+    | file :: files ->
+      if (SS.mem file resolved) 
+        then resolve_imports_3 files resolved path funcs
+        else (if (List.mem file path)
+                then raise (Error "cyclic dependency")
+                else (
+                  let file_contents = load_file file in
+                  let cur_prog = parse_e_prog file_contents in
+                  let cur_prog_funcs = E_Prog.get_funcs cur_prog in 
+                  resolve_imports_3 (cur_prog.imports :: to_resolve) resolved (file :: path) (funcs @ cur_prog_funcs) 
+                ))
+
+let my_resolve_imports (prog : E_Prog.t) : E_Func.t list = 
+   resolve_imports_3  [ prog.imports ] SS.empty [ prog.file_name ] (E_Prog.get_funcs prog)
