e6ed676661a6105aa5aa30b38d35bafbb8ff1901
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-08-15 18:54:31 +0100

    Add "file_name" property to the E_Prog record; Add functions to access and update the E_Prog properties;

--- implementation/lib/E_Prog.ml
+++ implementation/lib/E_Prog.ml
@@ -1,6 +1,7 @@
 type t = {
-  imports : string list;
-  funcs   : (string, E_Func.t) Hashtbl.t
+  mutable file_name : string;
+  imports           : string list;
+  funcs             : (string, E_Func.t) Hashtbl.t
 }
 
 let add_funcs (prog : t) (funcs : E_Func.t list) : unit =
@@ -9,17 +10,26 @@ let add_funcs (prog : t) (funcs : E_Func.t list) : unit =
       | Some _ -> invalid_arg ("Function \"" ^ f.name ^ "\" already exists in the program")
     ) funcs
 
-
 let create (imports : string list) (funcs : E_Func.t list) : t =
   let prog = {
+    file_name = "temporary name";
     imports;
-    funcs   = Hashtbl.create 511
+    funcs = Hashtbl.create 511
   } in
   add_funcs prog funcs;
   prog
 
+let get_file_name (prog : t) : string = prog.file_name
+
 let get_func (prog : t) (func : string) : E_Func.t = Hashtbl.find prog.funcs func
 
+let get_funcs (prog : t) : E_Func.t list = List.of_seq (Hashtbl.to_seq_values prog.funcs)
+
+let get_imports (prog : t) : string list = prog.imports
+
+let set_file_name (prog : t) (file_name : string) : unit = prog.file_name <- file_name
+
 let str (prog : t) : string =
+  prog.file_name ^ "\n" ^
   (String.concat " " (List.map (fun i -> "import " ^ i) prog.imports)) ^
   Hashtbl.fold (fun n v ac -> (if ac <> "" then ac ^ "\n" else ac) ^ (Printf.sprintf "(%s -> %s)" n (E_Func.str v))) prog.funcs ""
