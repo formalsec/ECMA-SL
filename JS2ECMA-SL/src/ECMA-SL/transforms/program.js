bbcd8e258a74062dc71c1b3758262b7f7179bfd9
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-10-04 17:35:49 +0100

    Identify the type of code that the object refers to; This helpful property is used in the implementation of the ES5 standard.

--- JS2ECMA-SL/src/ECMA-SL/transforms/program.js
+++ JS2ECMA-SL/src/ECMA-SL/transforms/program.js
@@ -21,6 +21,8 @@ module.exports = {
     obj.variableDeclarations = variableDeclarations;
     obj.functionDeclarations = functionDeclarations;
 
+    obj.codeType = "global";
+
     return obj;
   },
 };
