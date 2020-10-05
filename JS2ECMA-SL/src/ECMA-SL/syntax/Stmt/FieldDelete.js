adcace7d193fbaa270855dde827d6fc91348d9e7
FranciscoQuinaz <franciscoquinaz@tecnico.ulisboa.pt>
2020-10-05 01:06:45 +0100

    Heap labels - Not strongly tested

--- JS2ECMA-SL/src/ECMA-SL/syntax/Stmt/FieldDelete.js
+++ JS2ECMA-SL/src/ECMA-SL/syntax/Stmt/FieldDelete.js
@@ -1,14 +1,23 @@
 const Expr = require("../Expr/Expr");
-const EmptyLab = require("../Labels/EmptyLab");
+const FieldDeleteLab = require("../Labels/FieldDeleteLab");
 
 function MakeFieldDelete(Stmt){
 	 
 	class FieldDelete extends Stmt {
-	  constructor(expressionObject, expressionField) {
-	    super();
-	    this.expressionObject = expressionObject;
-	    this.expressionField = expressionField;
-	    }
+	  	constructor(expressionObject, expressionField) {
+		    super();
+		    this.expressionObject = expressionObject;
+		    this.expressionField = expressionField;
+		    }
+
+    	interpret(config){
+    		config.cont=config.cont.slice(1);
+    		var object = this.expressionObject.interpret(config.store).value;
+      		var field = this.expressionField.interpret(config.store).value;
+    		config.heap.deleteField(object, field);
+    	return {config : config, seclabel: new FieldDeleteLab(object, field)};
+
+    	}
 
 	}
 	FieldDelete.fromJSON = function(obj) {
