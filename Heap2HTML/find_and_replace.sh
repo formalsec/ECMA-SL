ab0ade36946b4515f98ced5fe243ba7a16d351af
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-10-14 22:30:43 +0100

    Update heap file location to match previous changes

--- Heap2HTML/find_and_replace.sh
+++ Heap2HTML/find_and_replace.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cp "../implementation/test/heap.json" .
+cp "../implementation/heap.json" .
 
 sed -i "s/'empty\b/\"'empty\"/g" heap.json
 sed -i "s/'undefined\b/\"'undefined\"/g" heap.json
