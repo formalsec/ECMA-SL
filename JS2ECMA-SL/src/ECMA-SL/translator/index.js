612c1a182d828ee800e949baa6859ea3ff030960
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-10-15 02:47:11 +0100

    Transform "Identifier" objects: - "undefined" should be transformed to "Literal" object; - update "translator" module accordingly;

--- JS2ECMA-SL/src/ECMA-SL/translator/index.js
+++ JS2ECMA-SL/src/ECMA-SL/translator/index.js
@@ -33,6 +33,10 @@ function translateNull() {
   return translateLiteral(new PrimitiveVal(null));
 }
 
+function translateUndefined() {
+  return translateLiteral(new PrimitiveVal(undefined));
+}
+
 function translateNumber(value) {
   if (Number.isInteger(value)) {
     if (Number.isSafeInteger(value)) {
@@ -87,7 +91,7 @@ function translateObject(obj) {
 function traverseAndTranslate(value) {
   switch (typeof value) {
     case "undefined":
-      throw new Error("The undefined value is not supported");
+      return translateUndefined();
     case "boolean":
       return translateBoolean(value);
     case "number":
