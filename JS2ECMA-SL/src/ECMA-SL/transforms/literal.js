b72c14c09fbd58e93fe9c425164f5bd2a3f94824
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-10-15 03:50:25 +0100

    Transform "Literal" objects: - transform a null literal in a string; - used to distinguish between the null of the language and the null value

---
+++ JS2ECMA-SL/src/ECMA-SL/transforms/literal.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transform: function (obj) {
+    if (obj.type !== "Literal") {
+      throw Error('Unexpected object type; Expecting "Literal"');
+    }
+
+    if (obj.value === null) {
+      obj.value = "null";
+      obj.raw = "\"null\""
+    }
+
+    return obj
+  }
+}
