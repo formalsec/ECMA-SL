732149ae2ade5ebf6a609f6c96695dc22e47b1d2
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-11-26 23:17:49 +0000

    Since the "Return" Core statements are expected to return a tuple, guarantee that only the second component is return has the final value.

--- implementation/lib/semantics/core/Core_Interpreter.ml
+++ implementation/lib/semantics/core/Core_Interpreter.ml
@@ -335,11 +335,15 @@ let rec  small_step_iter (interceptor: string -> Val.t list -> Expr.t list  -> (
   match stmts with
   | [] ->  raise(Except "Empty list")
   | s::stmts' ->  let (return, label) = eval_small_step interceptor prog state stmts' verbose s in
-                    (match return with
-                    | Finalv v ->  Finalv v
-                    | Errorv v -> Errorv v
-                    | Intermediate (state', stmts'') ->
-                      small_step_iter interceptor prog state' mon_state stmts'' verbose)
+    (match return with
+     | Finalv v -> (match v with
+         | Some v -> (match v with
+             | Tuple t -> Finalv (Some (List.nth t 1))
+             | _       -> Finalv (Some v))
+         | None   -> Finalv v)
+     | Errorv v -> Errorv v
+     | Intermediate (state', stmts'') ->
+       small_step_iter interceptor prog state' mon_state stmts'' verbose)
                    (*let mon_return : Mon.monitor_return = Mon.eval_small_step mon_state label in
                    (match mon_return with
                     | MReturn mon_state' -> (
