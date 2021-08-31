bfb1e6a0e28e4fb584b20606f5ed77edbfa1b6dd
jsilll <joao.freixial.silveira@tecnico.ulisboa.pt>
2021-08-31 13:22:06 +0100

    improved testing script

--- implementation/test_all.sh
+++ implementation/test_all.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
-BASE_COMMAND="./exec_test262_tests.sh -6 -r"
-PARENT_DIR="my_tests/"
-FILE_SUFIX="test_results.txt"
-FILE_SUFIX_FAIL="test_results_fail.txt"
-FILE_SUFIX_ERROR="test_results_error.txt"
+BASE_COMMAND="./exec_test262_tests.sh -6 -r";
+PARENT_DIR="my_tests/";
+FILE_SUFIX="test_results.txt";
+FILE_SUFIX_TEMP="test_results_temp.txt";
 
-REPLACE_QUERY='s|" ... [1m[0;31m[7mERROR[0m"||g'
+writeERROR() {
+	echo "########## ERROR ##########" > $1;
+}
 
-echo "Testing Map ..."
-${BASE_COMMAND} my_tests/Map | tee ${PARENT_DIR}map_${FILE_SUFIX}
-grep FAIL ${PARENT_DIR}map_${FILE_SUFIX} > ${PARENT_DIR}map_${FILE_SUFIX_FAIL}
-grep ERROR ${PARENT_DIR}map_${FILE_SUFIX} > ${PARENT_DIR}map_${FILE_SUFIX_ERROR}
-rm -rf ${PARENT_DIR}map_${FILE_SUFIX}
+writeFAIL() {
+	echo "########## FAIL ##########" >> $1;
+}
 
-# echo "Testing Number ..."
-# ${BASE_COMMAND} my_tests/Number > ${PARENT_DIR}number_${FILE_SUFIX} &&\
-# grep FAIL ${PARENT_DIR}number_${FILE_SUFIX} > ${PARENT_DIR}number_${FILE_SUFIX_FAIL}  &&\
-# grep ERROR ${PARENT_DIR}number_${FILE_SUFIX} > ${PARENT_DIR}number_${FILE_SUFIX_ERROR}
-# rm -rf ${PARENT_DIR}number_${FILE_SUFIX}
-# 
-# echo "Testing Math ..."
-# ${BASE_COMMAND} my_tests/Math > ${PARENT_DIR}math_${FILE_SUFIX} &&\
-# grep FAIL ${PARENT_DIR}math_${FILE_SUFIX} > ${PARENT_DIR}math_${FILE_SUFIX_FAIL} &&\
-# grep ERROR ${PARENT_DIR}math_${FILE_SUFIX} > ${PARENT_DIR}math_${FILE_SUFIX_ERROR}
-# rm -rf ${PARENT_DIR}math_${FILE_SUFIX}
+testSection() {
+	${BASE_COMMAND} my_tests/${1} > ${PARENT_DIR}${1,,}_${FILE_SUFIX_TEMP};
 
-# sed ${REPLACE_QUERY} ${PARENT_DIR}map_${FILE_SUFIX}
-# sed ${REPLACE_QUERY} ${PARENT_DIR}map_${FILE_SUFIX_FAIL}
-# sed ${REPLACE_QUERY} ${PARENT_DIR}map_${FILE_SUFIX_ERROR}
-# 
-# sed ${REPLACE_QUERY} ${PARENT_DIR}number_${FILE_SUFIX}
-# sed ${REPLACE_QUERY} ${PARENT_DIR}number_${FILE_SUFIX_FAIL}
-# sed ${REPLACE_QUERY} ${PARENT_DIR}number_${FILE_SUFIX_ERROR}
-# 
-# sed ${REPLACE_QUERY} ${PARENT_DIR}math_${FILE_SUFIX}
-# sed ${REPLACE_QUERY} ${PARENT_DIR}math_${FILE_SUFIX_FAIL}
-# sed ${REPLACE_QUERY} ${PARENT_DIR}math_${FILE_SUFIX_ERROR}
+	writeERROR ${PARENT_DIR}${1,,}_${FILE_SUFIX};
+	grep ERROR ${PARENT_DIR}${1,,}_${FILE_SUFIX_TEMP} >> ${PARENT_DIR}${1,,}_${FILE_SUFIX};
+
+	writeFAIL ${PARENT_DIR}${1,,}_${FILE_SUFIX};
+	grep FAIL ${PARENT_DIR}${1,,}_${FILE_SUFIX_TEMP} >> ${PARENT_DIR}${1,,}_${FILE_SUFIX};
+
+	echo "\n" >> ${PARENT_DIR}${1,,}_${FILE_SUFIX};
+	rm -rf ${PARENT_DIR}${1,,}_${FILE_SUFIX_TEMP};
+}
+
+
+for section in Map Number Math ArrayBuffer
+do
+	echo "Testing ${section} ..."
+	testSection $section
+done
