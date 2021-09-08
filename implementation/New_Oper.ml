fc475e740fde9b465810e2583cd30ed5570bf89d
jsilll <joao.freixial.silveira@tecnico.ulisboa.pt>
2021-09-08 17:57:07 +0100

    started bin32/64 impl

---
+++ implementation/New_Oper.ml
@@ -0,0 +1,32 @@
+let float64_to_le_bytes (v : Val.t) : Val.t = match v with
+  | Flt x -> 
+    let v = Int64.bits_of_float x in
+    for i = 0 to 7 do
+      Printf.printf "%Lx " (Int64.logand 255L (Int64.shift_right v (i * 8))) ;
+    done
+    Flt (x)
+  | _      -> invalid_arg "Exception in Oper.float64_to_le_bytes: this operation is only applicable to Float arguments"
+
+let float64_to_be_bytes (v : Val.t) : Val.t = match v with
+  | Flt x -> 
+    let v = Int64.bits_of_float x in
+    for i = 0 to 7 do
+      Printf.printf "%Lx " (Int64.logand 255L (Int64.shift_right v ((7 - i) * 8))) ;
+    done
+  | _      -> invalid_arg "Exception in Oper.float64_to_be_bytes: this operation is only applicable to Float arguments"
+
+let float32_to_le_bytes (v : Val.t) : Val.t = match v with
+  | Flt x -> 
+    let v = Int32.bits_of_float x in
+    for i = 0 to 7 do
+      Printf.printf "%Lx " (Int64.logand 255l (Int64.shift_right v (i * 8))) ;
+    done
+  | _      -> invalid_arg "Exception in Oper.float32_to_le_bytes: this operation is only applicable to Float arguments"
+
+let float32_to_be_bytes (v : Val.t) : Val.t = match v with
+  | Flt x -> 
+    let v = Int32.bits_of_float x in
+    for i = 0 to 7 do
+      Printf.printf "%Lx " (Int64.logand 255l (Int64.shift_right v ((7 - i) * 8))) ;
+    done
+  | _      -> invalid_arg "Exception in Oper.float32_to_be_bytes: this operation is only applicable to Float arguments"
