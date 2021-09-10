ede244d79c3b6620c775a6e31a58ff2f4692d02f
Th0mz <tomastavares21122001@gmail.com>
2021-09-10 14:25:49 +0100

    es6 single

---
+++ implementation/exec_es6_single_test.sh
@@ -0,0 +1,6 @@
+node ../JS2ECMA-SL/src/index.js -i $1 -o ast.cesl
+./main.native -mode c -i ES6_interpreter/main.esl -o core.cesl 
+echo "; " >> core.cesl 
+cat ast.cesl >> core.cesl
+./main.native -mode ci -i core.cesl > result2.txt
+rm core.cesl ast.cesl
