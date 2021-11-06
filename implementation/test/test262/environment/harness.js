1ea4a8fa63f099b028220b8910306e1cb183d0d3
NNate1 <nuno.policarpo@tecnico.ulisboa.pt>
2021-11-06 09:59:07 +0000

    DetachedBuffer.js added to harness

--- implementation/test/test262/environment/harness.js
+++ implementation/test/test262/environment/harness.js
@@ -387,3 +387,11 @@ function verifyProperty(obj, name, desc, options) {
 
     return true;
 }
+
+
+function $DETACHBUFFER(buffer) {
+  if (!$262 || typeof $262.detachArrayBuffer !== "function") {
+    throw new Test262Error("No method available to detach an ArrayBuffer");
+  }
+  $262.detachArrayBuffer(buffer);
+}
