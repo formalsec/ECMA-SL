9190e4194d67039b527e719626baccd1c3f1a9ff
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2022-12-19 19:00:41 +0000

    ECMA-SL: added --version option and made External.ml use the input's name on aux files

---
+++ ECMA-SL/main/flags.ml
@@ -0,0 +1,8 @@
+let file = ref ""
+let heap_file = ref ""
+let mode = ref ""
+let mon = ref ""
+let output = ref ""
+let verbose = ref false
+let silent = ref false
+let parse = ref false
--- ECMA-SL/semantics/core/External.ml
+++ ECMA-SL/semantics/core/External.ml
@@ -1,22 +1,20 @@
-
-
-let aux_input_file = "__parse_js_aux_input_file__.js"
-let aux_output_file = "__parse_js_aux_output_file__.js"
-
 let eval_build_ast_func = String_Utils.make_fresh_var_generator "eval_func_"
 
 let parseJS
     (prog  : Prog.t)
     (heap  : Heap.t)
     (str   : string) : Val.t =
-
-  File_Utils.burn_to_disk aux_input_file str;
+  let base = Filename.basename !Flags.file in
+  let input_file = "__parse_in_" ^  base ^ "__.js"
+  and output_file = "__parse_out_" ^ base ^ "__.js" in
+  File_Utils.burn_to_disk input_file str;
   let func_id = eval_build_ast_func () in
-  let command = Printf.sprintf "node ../JS2ECMA-SL/src/index.js -c -i %s -o %s -n %s" aux_input_file aux_output_file func_id in
+  let command = Printf.sprintf "node ../JS2ECMA-SL/src/index.js -c -i %s -o %s -n %s" input_file output_file func_id in
   let _ = Sys.command command in
-  let parsed_str = Parsing_Utils.load_file aux_output_file in
+  let parsed_str = Parsing_Utils.load_file output_file in
   let func = Parsing_Utils.parse_func parsed_str in
   Prog.add_func prog func_id func;
+  List.iter Sys.remove [input_file; output_file];
   Val.Str func_id
 
 let loadInitialHeap
