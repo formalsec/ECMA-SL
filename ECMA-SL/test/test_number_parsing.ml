ae02b48cb8d105bf7d564b224b8c9f1b6aede2b3
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2022-12-24 14:36:34 +0000

    added number_parsing_utils test for is_white_space function

--- ECMA-SL/test/test_number_parsing.ml
+++ ECMA-SL/test/test_number_parsing.ml
@@ -1,12 +1,17 @@
 open Number_Parsing_Utils
 
 let assert_equals x y =
-  print_float x;
-  print_float y;
   if x <> y then
     raise (Failure (Printf.sprintf "Expected: %.9f, but got %.9f." x y))
+
+let test_is_white_space () =
+  assert (is_white_space ' ');
+  assert (is_white_space '\n');
+  assert (is_white_space '\t')
 
 let test_parse_string_numeric_literal () =
   assert_equals (parse_string_numeric_literal "10.123214123") 10.123214123
 
-let () = test_parse_string_numeric_literal ()
+let () =
+  test_is_white_space ();
+  test_parse_string_numeric_literal ()
