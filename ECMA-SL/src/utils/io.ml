be964e59758d335294b4b12524017ca2414a9ffd
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-12-06 23:57:41 +0000

    Protect file operations

--- ECMA-SL/src/utils/io.ml
+++ ECMA-SL/src/utils/io.ml
@@ -3,13 +3,14 @@ let safe_mkdir (dir : string) : unit =
 
 let write_file ~(file : string) ~(data : string) : unit =
   let oc = open_out file in
-  output_string oc data;
-  close_out oc
+  Fun.protect ~finally:(fun () -> close_out oc) (fun () -> output_string oc data)
 
 let load_file (file : string) : string =
   let ic = open_in file in
-  let n = in_channel_length ic in
-  let s = Bytes.create n in
-  really_input ic s 0 n;
-  close_in ic;
-  Bytes.to_string s
+  Fun.protect
+    ~finally:(fun () -> close_in ic)
+    (fun () ->
+      let n = in_channel_length ic in
+      let s = Bytes.create n in
+      really_input ic s 0 n;
+      Bytes.to_string s )
