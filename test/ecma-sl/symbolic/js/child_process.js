53b57999e42f1242815165bc538ce294925cf252
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-02 19:14:56 +0000

    Refactor js tests

--- test/ecma-sl/symbolic/js/child_process.js
+++ test/ecma-sl/symbolic/js/child_process.js
@@ -1,5 +1,5 @@
-var esl_symbolic = require("esl_symbolic");
-var child_process = require("child_process");
+let esl_symbolic = require("esl_symbolic");
+let child_process = require("child_process");
 
 let source = esl_symbolic.string("source");
 child_process.exec(source);
