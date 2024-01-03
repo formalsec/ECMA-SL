a21253b65e03fecd03475f4db50465cb41def59e
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-03 22:33:15 +0000

    Changes default hashtbl sz to 16 since it's already the default bin size in hashtbl.ml

--- ECMA-SL/config.ml
+++ ECMA-SL/config.ml
@@ -1,7 +1,7 @@
 let file = ref ""
 let target = ref "main"
 let workspace = ref "output"
-let default_hashtbl_sz = ref 0
+let default_hashtbl_sz = ref 16
 
 module Eslerr = struct
   let show_code = ref true
