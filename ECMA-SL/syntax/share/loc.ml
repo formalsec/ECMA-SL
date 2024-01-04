d0b4939a58afb7eb82fe30885c6af4875f524fa6
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-04 09:49:16 +0000

    Loc: fix memory leak, add pp function

--- ECMA-SL/syntax/share/loc.ml
+++ ECMA-SL/syntax/share/loc.ml
@@ -1,10 +1,10 @@
 type t = string
 
-let count = ref 0
-
-let inc_get_count () : int =
-  incr count;
-  !count
+let inc_get_count =
+  let count = ref 0 in
+  fun () ->
+    incr count;
+    !count
 
 let create () : t = Fmt.sprintf "$loc_%d" (inc_get_count ())
 let pp (fmt : Fmt.t) (l : t) : unit = Fmt.fprintf fmt "%S" l
