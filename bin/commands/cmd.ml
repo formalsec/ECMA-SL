4cb0af76aecd2b80d30f351ab33db7f10112b50b
andreffnascimento <andreffnascimento@outlook.com>
2024-01-05 10:02:40 +0000

    fix(bin): fix error header for custom esl errors

--- bin/commands/cmd.ml
+++ bin/commands/cmd.ml
@@ -7,24 +7,24 @@ exception Command_error of error
 let error_code (error : error) : int =
   match error with Failure -> 1 | Error -> 2
 
-let log ?(esl_header : bool = true) (msg : string) : unit =
-  if esl_header then Printf.eprintf "%s: %s\n" Sys.argv.(0) msg
-  else Printf.eprintf "%s\n" msg
+let log ?(header : bool = true) msg_fmt =
+  let header_str = if header then "ecma-sl: " else "" in
+  Format.(kdprintf (eprintf "%s%t" header_str) msg_fmt)
 
 let eval_cmd (cmd : unit -> unit) : int =
   let open Ecma_sl in
   try cmd () |> fun () -> 0 with
   | Eslerr.Internal_error _ as exn ->
     flush_all ();
-    log (Eslerr.format exn);
+    log "%s" (Eslerr.format exn);
     Printexc.print_backtrace stderr;
     error_code Failure
   | Eslerr.Runtime_error _ as exn ->
-    log (Eslerr.format exn);
+    log ~header:false "%s" (Eslerr.format exn);
     error_code Error
   | Command_error err -> error_code err
 
 let test_file_ext (file : string) (exts : string list) : unit =
   if not (List.exists (Filename.check_suffix file) exts) then
     let ext_str = String.concat " or " exts in
-    log (Printf.sprintf "expecting file with '%s' extension" ext_str)
+    log "expecting file with '%s' extension" ext_str
