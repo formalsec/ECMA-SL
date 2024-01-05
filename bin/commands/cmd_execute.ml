9b22798b3523ad5d27a9e6a24932258c6144649f
andreffnascimento <andreffnascimento@outlook.com>
2024-01-05 15:11:07 +0000

    chore(bin): add colored output option

--- bin/commands/cmd_execute.ml
+++ bin/commands/cmd_execute.ml
@@ -3,8 +3,8 @@ let run (_input_file : string) (_execution_lang : Lang.t)
   unit =
   print_endline "NOT IMPLEMENTED!"
 
-let main (_debug : bool) (input_file : string) (execution_lang : Lang.t)
-  (ecmaref_version : Ecmaref.version) (ecmaref_builder : Ecmaref.builder) : int
-    =
+let main (_copts : Options.common_options) (input_file : string)
+  (execution_lang : Lang.t) (ecmaref_version : Ecmaref.version)
+  (ecmaref_builder : Ecmaref.builder) : int =
   let run' () = run input_file execution_lang ecmaref_version ecmaref_builder in
   Cmd.eval_cmd run'
