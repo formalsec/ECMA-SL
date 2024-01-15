3dcdce3d1aab023b3b14eb0b63e9cf91bd5884a9
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-15 10:43:41 +0000

    Remove some allocations in value_typing

--- ECMA-SL/functorial/value_typing.ml
+++ ECMA-SL/functorial/value_typing.ml
@@ -1,117 +1,191 @@
-open Args_typing_unop
-open Args_typing_binop
-open Type_of_val
+module Value = Symbolic_value.M
+(* TODO: this is to remove when we mirgate to the encoding values *)
 
-let type_of_unop (op : Operator.unopt) (arg_t : Type.t option) : Type.t option =
+let type_of_neg = function
+  | Type.IntType -> Some Type.IntType
+  | Type.FltType -> Some Type.FltType
+  | _ -> None
+
+let type_of_bitwise_not = function
+  | Type.IntType -> Some Type.IntType
+  | _ -> None
+
+let type_of_not = function Type.BoolType -> Some Type.BoolType | _ -> None
+
+let type_of_len (container : Type.t) (ty : Type.t) : Type.t option =
+  match (container, ty) with
+  | (Type.ListType, Type.ListType) -> Some Type.IntType
+  | (Type.TupleType, Type.TupleType) -> Some Type.IntType
+  | _ -> None
+
+let on_int ~(return : Type.t) = function
+  | Type.IntType -> Some return
+  | _ -> None
+
+let on_float ~(return : Type.t) = function
+  | Type.FltType -> Some return
+  | _ -> None
+
+let type_of_sconcat = function Type.ListType -> Some Type.StrType | _ -> None
+
+let type_of_binary_arith (ty1 : Type.t) (ty2 : Type.t) : Type.t option =
+  match (ty1, ty2) with
+  | (Type.IntType, Type.IntType) -> Some Type.IntType
+  | (Type.FltType, Type.IntType) -> Some Type.FltType
+  | (Type.IntType, Type.FltType) -> Some Type.FltType
+  | (Type.FltType, Type.FltType) -> Some Type.FltType
+  | _ -> None
+
+let type_of_eq (ty1 : Type.t) (ty2 : Type.t) : Type.t option =
+  (* FIXME: This is useless? *)
+  match (ty1, ty2) with
+  | (Type.IntType, Type.IntType)
+  | (Type.FltType, Type.FltType)
+  | (Type.ArrayType, Type.ArrayType)
+  | (Type.StrType, Type.StrType)
+  | (Type.BoolType, Type.BoolType) ->
+    Some Type.BoolType
+  (* missing the symbolic cases *)
+  | _ ->
+    (* None *)
+    Some Type.BoolType
+
+let type_of_relop (ty1 : Type.t) (ty2 : Type.t) : Type.t option =
+  match (ty1, ty2) with
+  | (Type.IntType, Type.IntType) -> Some Type.BoolType
+  | (Type.FltType, Type.FltType) -> Some Type.BoolType
+  | _ -> None
+
+let type_of_binary_logic (arg1_t : Type.t) (arg2_t : Type.t) : Type.t option =
+  match (arg1_t, arg2_t) with
+  | (Type.BoolType, Type.BoolType) -> Some Type.BoolType
+  | _ -> None
+
+let bitwise_operators_typing_logic (ty1 : Type.t) (ty2 : Type.t) : Type.t option
+    =
+  match (ty1, ty2) with
+  | (Type.FltType, Type.FltType) -> Some Type.FltType
+  | _ -> None
+
+let bin_args_typing_inlist (arg1_t : Type.t) : Type.t option =
+  match arg1_t with Type.ListType -> Some Type.BoolType | _ -> None
+
+let bin_args_typing_lnth (arg1 : Value.value) (arg2 : Value.value) :
+  Type.t option =
+  let open Value in
+  match (arg1, arg2) with
+  | (Val (Val.List l), Val (Val.Int i)) -> Val.type_of (List.nth l i)
+  | (_, _) -> None
+
+let bin_args_typing_pow (arg1_t : Type.t) (arg2_t : Type.t) : Type.t option =
+  match (arg1_t, arg2_t) with
+  | (Type.FltType, Type.FltType) -> Some Type.FltType
+  | _ -> None
+
+let type_of_unop (op : Operator.unopt) (ty : Type.t) : Type.t option =
   match op with
-  | Operator.Neg -> un_args_typing_neg arg_t
-  | Operator.LogicalNot -> un_args_typing_not arg_t
+  | Operator.Neg -> type_of_neg ty
+  | Operator.LogicalNot -> type_of_not ty
+  | Operator.BitwiseNot -> type_of_bitwise_not ty
   | Operator.ListHead -> None
   | Operator.ListTail -> None
   | Operator.TupleFirst -> None
   | Operator.TupleSecond -> None
-  | Operator.TupleLen -> un_args_typing_container_length arg_t Type.TupleType
-  | Operator.IntToFloat -> un_args_typing_int_casts arg_t Type.FltType
-  | Operator.IntToString -> un_args_typing_int_casts arg_t Type.StrType
-  | Operator.FloatToString -> un_args_typing_float_casts arg_t Type.StrType
+  | Operator.TupleLen -> type_of_len Type.TupleType ty
+  | Operator.IntToFloat -> on_int ~return:Type.FltType ty
+  | Operator.IntToString -> on_int ~return:Type.StrType ty
+  | Operator.FloatToString -> on_float ~return:Type.StrType ty
   | Operator.StringToFloat -> Some Type.FltType
   | Operator.ToUint32 -> Some Type.FltType
-  | Operator.Typeof -> un_args_typing_typeof arg_t
-  | Operator.StringConcat -> un_args_typing_sconcat arg_t
-  | Operator.Exp -> un_args_typing_float_math arg_t Type.FltType
-  | Operator.LogE -> un_args_typing_float_math arg_t Type.FltType
-  | Operator.Log10 -> un_args_typing_float_math arg_t Type.FltType
-  | Operator.Ceil -> un_args_typing_float_math arg_t Type.FltType
-  | Operator.Floor -> un_args_typing_float_math arg_t Type.FltType
-  | Operator.Abs -> un_args_typing_float_math arg_t Type.FltType
-  | Operator.ToInt -> un_args_typing_float_math arg_t Type.FltType
-  | Operator.Sqrt -> un_args_typing_float_math arg_t Type.FltType
-  | Operator.IsNaN -> un_args_typing_float_math arg_t Type.BoolType
+  | Operator.Typeof -> Some Type.TypeType
+  | Operator.StringConcat -> type_of_sconcat ty
+  | Operator.Exp -> on_float ~return:Type.FltType ty
+  | Operator.LogE -> on_float ~return:Type.FltType ty
+  | Operator.Log10 -> on_float ~return:Type.FltType ty
+  | Operator.Ceil -> on_float ~return:Type.FltType ty
+  | Operator.Floor -> on_float ~return:Type.FltType ty
+  | Operator.Abs -> on_float ~return:Type.FltType ty
+  | Operator.ToInt -> on_float ~return:Type.FltType ty
+  | Operator.Sqrt -> on_float ~return:Type.FltType ty
+  | Operator.IsNaN -> on_float ~return:Type.BoolType ty
   | Operator.StringLen -> Some Type.IntType
   | Operator.StringLenU -> Some Type.IntType
-  | Operator.BitwiseNot -> un_args_typing_bitwise_not arg_t
   | Operator.Trim -> Some Type.StrType
   | Operator.ListLen -> Some Type.IntType
   | Operator.ListSort -> Some Type.ListType
   | _ ->
-    failwith
-      ( "Typing Error: [type_of_unop] -> unsuported typing for unary operation "
-      ^ Operator.str_of_unopt_single op )
+    Log.err
+      "Typing Error: [type_of_unop] -> unsuported typing for unary operation %a"
+      Operator.pp_of_unopt_single op
 
-let type_of_binop (op : Operator.binopt) (arg1 : Symbolic_value.M.value)
-  (arg2 : Symbolic_value.M.value) (arg1_t : Type.t option)
-  (arg2_t : Type.t option) : Type.t option =
+let type_of_binop (op : Operator.binopt) (v1 : Value.value) (v2 : Value.value)
+  (ty1 : Type.t) (ty2 : Type.t) : Type.t option =
   match op with
-  | Operator.Plus -> bin_args_typing_arith arg1_t arg2_t
-  | Operator.Minus -> bin_args_typing_arith arg1_t arg2_t
-  | Operator.Times -> bin_args_typing_arith arg1_t arg2_t
-  | Operator.Div -> bin_args_typing_arith arg1_t arg2_t
-  | Operator.Modulo -> bin_args_typing_arith arg1_t arg2_t
-  | Operator.Eq -> bin_args_typing_eq arg1_t arg2_t
-  | Operator.Lt -> bin_args_typing_comp arg1_t arg2_t
-  | Operator.Gt -> bin_args_typing_comp arg1_t arg2_t
-  | Operator.Le -> bin_args_typing_comp arg1_t arg2_t
-  | Operator.Ge -> bin_args_typing_comp arg1_t arg2_t
-  | Operator.LogicalAnd -> bin_args_typing_logic arg1_t arg2_t
-  | Operator.LogicalOr -> bin_args_typing_logic arg1_t arg2_t
-  | Operator.Min -> bin_args_typing_arith arg1_t arg2_t
-  | Operator.Max -> bin_args_typing_arith arg1_t arg2_t
+  | Operator.Plus -> type_of_binary_arith ty1 ty2
+  | Operator.Minus -> type_of_binary_arith ty1 ty2
+  | Operator.Times -> type_of_binary_arith ty1 ty2
+  | Operator.Div -> type_of_binary_arith ty1 ty2
+  | Operator.Modulo -> type_of_binary_arith ty1 ty2
+  | Operator.Eq -> type_of_eq ty1 ty2
+  | Operator.Lt -> type_of_relop ty1 ty2
+  | Operator.Gt -> type_of_relop ty1 ty2
+  | Operator.Le -> type_of_relop ty1 ty2
+  | Operator.Ge -> type_of_relop ty1 ty2
+  | Operator.LogicalAnd -> type_of_binary_logic ty1 ty2
+  | Operator.LogicalOr -> type_of_binary_logic ty1 ty2
+  | Operator.Min -> type_of_binary_arith ty1 ty2
+  | Operator.Max -> type_of_binary_arith ty1 ty2
   | Operator.TupleNth -> None
-  | Operator.ListNth -> bin_args_typing_lnth arg1 arg2
-  | Operator.ListMem -> bin_args_typing_inlist arg1_t
+  | Operator.ListNth -> bin_args_typing_lnth v1 v2
+  | Operator.ListMem -> bin_args_typing_inlist ty1
   | Operator.ListPrepend -> None
   | Operator.ListAdd -> None
-  | Operator.Pow -> bin_args_typing_pow arg1_t arg2_t
+  | Operator.Pow -> bin_args_typing_pow ty1 ty2
   | Operator.StringNth -> Some Type.StrType
   | Operator.StringNthU -> Some Type.StrType
   | Operator.BitwiseAnd | Operator.BitwiseOr | Operator.BitwiseXor
   | Operator.ShiftLeft | Operator.ShiftRight | Operator.ShiftRightLogical ->
-    bitwise_operators_typing_logic arg1_t arg2_t
+    bitwise_operators_typing_logic ty1 ty2
   | _ ->
-    failwith
-      ( "Typing Error: [type_of_binop] -> unsuported typing for binary \
-         operation "
-      ^ Operator.str_of_binopt_single op )
+    Log.err "Typing Error: [type_of_binop] -> unsuported typing for %a"
+      Operator.pp_of_binopt_single op
 
-let type_of_triop (op : Operator.triopt) (_arg1_t : Type.t option)
-  (arg2_t : Type.t option) (arg3_t : Type.t option) : Type.t option =
+let type_of_triop (op : Operator.triopt) (_ : Type.t) (ty2 : Type.t)
+  (ty3 : Type.t) : Type.t option =
   match op with
   | Operator.StringSubstr -> Some Type.StrType
   | Operator.StringSubstrU -> Some Type.StrType
   | Operator.ListSet -> Some Type.ListType
   | Operator.ArraySet -> Some Type.ArrayType
   | Operator.ITE -> (
-    match (arg2_t, arg3_t) with
-    | (Some t2, Some t3) -> (
-      if t2 = t3 then arg2_t
-      else
-        match (t2, t3) with
-        | (_, Type.SymbolType) -> arg2_t
-        | (Type.SymbolType, _) -> arg3_t
-        | _ ->
-          Format.kasprintf failwith "types don't match for ITE: %s %s\n"
-            (Type.str t2) (Type.str t3) )
-    | _ -> failwith "types don't match for ITE." )
+    if ty2 = ty3 then Some ty2
+    else
+      match (ty2, ty3) with
+      | (_, Type.SymbolType) -> Some ty2
+      | (Type.SymbolType, _) -> Some ty3
+      | _ ->
+        Log.err "types don't match for ITE: %a %a@." Type.pp ty2 Type.pp ty3 )
 
 let rec type_of (v : Symbolic_value.M.value) : Type.t option =
-  let open Symbolic_value.M in
+  let open Value in
+  let open Syntax.Option in
   match v with
-  | Val v -> type_of_val v
-  | UnOpt (op, arg) ->
-    let arg_t = type_of arg in
-    type_of_unop op arg_t
-  | BinOpt (op, arg1, arg2) ->
-    let arg1_t = type_of arg1
-    and arg2_t = type_of arg2 in
-    type_of_binop op arg1 arg2 arg1_t arg2_t
-  | TriOpt (op, arg1, arg2, arg3) ->
-    let arg1_t = type_of arg1
-    and arg2_t = type_of arg2
-    and arg3_t = type_of arg3 in
-    type_of_triop op arg1_t arg2_t arg3_t
+  | Val v -> Val.type_of v
+  | UnOpt (op, v) ->
+    let* ty = type_of v in
+    type_of_unop op ty
+  | BinOpt (op, v1, v2) ->
+    let* ty1 = type_of v1 in
+    let* ty2 = type_of v2 in
+    type_of_binop op v1 v2 ty1 ty2
+  | TriOpt (op, v1, v2, v3) ->
+    let* ty1 = type_of v1 in
+    let* ty2 = type_of v2 in
+    let* ty3 = type_of v3 in
+    type_of_triop op ty1 ty2 ty3
   | Symbolic (t, _) -> Some t
   | NOpt (Operator.ListExpr, _) -> Some Type.ListType
   | NOpt (Operator.TupleExpr, _) -> Some Type.TupleType
   | NOpt (Operator.ArrayExpr, _) -> Some Type.ArrayType
   | Curry _ -> Some Type.CurryType
-  | _ -> Format.kasprintf failwith "%a: Not typed!" Pp.pp v
+  | _ -> Log.err "%a: Not typed!" Pp.pp v
