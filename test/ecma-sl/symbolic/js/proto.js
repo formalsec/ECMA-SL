31ac5ff9efc052ec11d726344c7367cfb4b65276
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-23 20:23:00 +0000

    Promote tests

--- test/ecma-sl/symbolic/js/proto.js
+++ test/ecma-sl/symbolic/js/proto.js
@@ -1,3 +1,9 @@
 let obj = {};
 obj.__proto__.foo = "bar";
 console.log(({}).foo);
+let obj2 = { "__proto__" : { bar : "baz" } };
+console.log(({}).bar);
+console.log(obj2.bar);
+let obj3 = { ["__proto__"] : { baz : "qux" } };
+console.log(obj3.baz);
+console.log(obj3.__proto__.baz);
