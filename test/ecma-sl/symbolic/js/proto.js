d118f280c22e1d9c88d0e1c6f2267899530ffd8d
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-30 20:39:22 +0000

    Test computed properties

--- test/ecma-sl/symbolic/js/proto.js
+++ test/ecma-sl/symbolic/js/proto.js
@@ -4,6 +4,7 @@ console.log(({}).foo);
 let obj2 = { "__proto__" : { bar : "baz" } };
 console.log(({}).bar);
 console.log(obj2.bar);
-let obj3 = { ["__proto__"] : { baz : "qux" } };
+let x = "__proto__";
+let obj3 = { [x] : { baz : "qux" } };
 console.log(obj3.baz);
 console.log(obj3.__proto__.baz);
