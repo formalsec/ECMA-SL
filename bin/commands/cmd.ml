02e77108421677732ba1f72231194d8722b8a1b0
andreffnascimento <andreffnascimento@outlook.com>
2024-01-31 21:05:36 +0000

    chore(bin): clean the error esl logger

--- bin/commands/cmd.ml
+++ bin/commands/cmd.ml
@@ -26,6 +26,24 @@ let log ?(header : bool = true) msg_fmt =
   let header_str = if header then "ecma-sl: " else "" in
   Format.(kdprintf (eprintf "%s%t@." header_str) msg_fmt)
 
+let esl_internal_err (exn : exn) : int =
+  flush_all ();
+  log "%a" Ecma_sl.Eslerr.pp exn;
+  Printexc.print_backtrace stderr;
+  code_of_exn exn |> error_code
+
+let esl_default_err (exn : exn) : int =
+  log ~header:false "%a" Ecma_sl.Eslerr.pp exn;
+  code_of_exn exn |> error_code
+
+let eval_cmd (cmd : unit -> unit) : int =
+  let open Ecma_sl in
+  try cmd () |> fun () -> 0 with
+  | Eslerr.Internal_error _ as exn -> esl_internal_err exn
+  | Eslerr.Compile_error _ as exn -> esl_default_err exn
+  | Eslerr.Runtime_error _ as exn -> esl_default_err exn
+  | Command_error err -> error_code err
+
 let test_file_ext (langs : Lang.t list) (file : string) : Lang.t =
   match Lang.test_file_ext langs (Filename.extension file) with
   | Some lang -> lang
@@ -35,16 +53,3 @@ let test_file_ext (langs : Lang.t list) (file : string) : Lang.t =
     let pp_lst seq pp fmt lst = pp_print_list ~pp_sep:(pp_sep seq) pp fmt lst in
     log "expecting file extensions: { %a }" (pp_lst " ; " Lang.pp) langs;
     raise (Command_error Error)
-
-let eval_cmd (cmd : unit -> unit) : int =
-  let open Ecma_sl in
-  try cmd () |> fun () -> 0 with
-  | Eslerr.Internal_error _ as exn ->
-    flush_all ();
-    log "%a" Eslerr.pp exn;
-    Printexc.print_backtrace stderr;
-    code_of_exn exn |> error_code
-  | (Eslerr.Compile_error _ | Eslerr.Runtime_error _) as exn ->
-    log ~header:false "%a" Eslerr.pp exn;
-    code_of_exn exn |> error_code
-  | Command_error err -> error_code err
