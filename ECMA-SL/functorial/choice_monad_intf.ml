f1ae0449bde7fec7748e4aca211a8e99d8ecf9a3
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-02-10 00:27:30 +0000

    Adds map to choice monad

--- ECMA-SL/functorial/choice_monad_intf.ml
+++ ECMA-SL/functorial/choice_monad_intf.ml
@@ -1,20 +1,20 @@
 module type Base = sig
   module V : Value_intf.T
 
+  type thread
   type 'a t
 
   val return : 'a -> 'a t
+  val run : 'a t -> thread -> ('a * thread) list
   val bind : 'a t -> ('a -> 'b t) -> 'b t
-  val check : V.value -> bool t
-  val check_add_true : V.value -> bool t
-  val branch : V.value -> bool t
-  val error : string -> 'a t
+  val map : 'a t -> ('a -> 'b) -> 'b t
 end
 
 module type Complete = sig
   include Base
 
-  type thread
-
-  val run : 'a t -> thread -> ('a * thread) list
+  val check : V.value -> bool t
+  val check_add_true : V.value -> bool t
+  val branch : V.value -> bool t
+  val error : string -> 'a t
 end
