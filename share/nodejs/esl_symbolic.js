274c1d09faaf3e7dcdf0804fb830c99fc2bd86ff
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-02-13 18:16:42 +0000

    Polluted side-effect

--- share/nodejs/esl_symbolic.js
+++ share/nodejs/esl_symbolic.js
@@ -5,6 +5,7 @@ if (!fs.existsSync(witness)) {
   console.log(`Non-existent witness file '${witness}'`);
   process.exit(1);
 }
+
 const symbolic_map = require(witness).symbolic_map;
 if (symbolic_map === undefined) {
   console.log(`Unable to load symbolic_map from '${witness}'`)
@@ -12,19 +13,34 @@ if (symbolic_map === undefined) {
 }
 
 function get(x) { return symbolic_map[x]; }
+
 function ignore(_x) { }
+
 function is_symbolic(_x) { return false }
+
 function lazy_object() { return {}; }
 
-module.exports =
-  { any: get
-  , number: get
-  , string: get
-  , boolean: get
-  , function: function(_x) { return function() { }; }
-  , lazy_object: lazy_object
-  , assume: ignore
-  , assert: ignore
-  , is_symbolic: is_symbolic
-  , sealProperties: ignore
+function polluted_object(depth) {
+  switch (depth) {
+    case 2:
+      return { ["__proto__"]: { "toString": "polluted" } }
+    case 3:
+      return { "constructor": { "prototype": { "toString": "polluted" } } }
+    default:
   }
+  return {}
+}
+
+module.exports = {
+  any: get,
+  number: get,
+  string: get,
+  boolean: get,
+  function: function(_x) { return function() { }; },
+  lazy_object: lazy_object,
+  assume: ignore,
+  assert: ignore,
+  is_symbolic: is_symbolic,
+  sealProperties: ignore,
+  polluted_object: polluted_object
+};
