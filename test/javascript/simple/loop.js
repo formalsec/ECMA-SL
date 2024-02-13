f8f9a3fc4ef41ceb9c07d1a52b2d156f6164cc98
andreffnascimento <andreffnascimento@outlook.com>
2024-02-13 12:55:54 +0000

    chore(ecmaref): add discriminant to match statements

---
+++ test/javascript/simple/loop.js
@@ -0,0 +1,14 @@
+/**
+ * * javaScript/simple/loop.js *
+ * 
+ * Loop statement that iterates 1000 times and returns the sum produced.
+ * 
+ * ?Expected result:	("C", 'normal, 499500.0, 'empty)
+ * ?Expected prints:	-
+*/
+
+let x = 0;
+for (let i = 0; i < 1000; i++) {
+	x += i;
+}
+x
