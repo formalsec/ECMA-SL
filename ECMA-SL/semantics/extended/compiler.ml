915d2a124248eb4c119f692c28a003f7a4c67ad3
andreffnascimento <andreffnascimento@outlook.com>
2024-02-13 15:06:51 +0000

    test(ecmaref): promote tests

--- ECMA-SL/semantics/extended/compiler.ml
+++ ECMA-SL/semantics/extended/compiler.ml
@@ -293,7 +293,7 @@ let rec compile_stmt (s : EStmt.t) : c_stmt =
   | ForEach (x, e, s', _, _) -> compile_foreach s.at x e s'
   | RepeatUntil (s', e, _) -> compile_repeatuntil s.at s' e
   | Switch (e, css, dflt, _) -> compile_switch s.at e css dflt
-  | MatchWith (e, disc, css) -> compile_matchwith s.at e disc css
+  | MatchWith (e, dsc, css) -> compile_matchwith s.at e dsc css
   | Lambda (x, lid, _, ctxvars, _) -> compile_lambdacall s.at x lid ctxvars
   | MacroApply (_, _) ->
     Eslerr.(internal __FUNCTION__ (UnexpectedEval (Some "MacroApply")))
