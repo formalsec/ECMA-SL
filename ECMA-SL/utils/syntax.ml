d27050cd0e249c634f2609f0225eddc8ccc79772
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-02-15 11:42:20 +0000

    Correctly exit on failure

--- ECMA-SL/utils/syntax.ml
+++ ECMA-SL/utils/syntax.ml
@@ -7,12 +7,34 @@ module Result = struct
   let ( let* ) v f = Result.bind v f
   let ( let+ ) v f = Result.map f v
 
+  (* Taken from: https://github.com/OCamlPro/owi/blob/main/src/syntax.ml *)
   let list_map ~f l =
-    let exception E of string in
+    let exit = ref None in
+    let exception Exit in
     try
       Ok
         (List.map
-           (fun v -> match f v with Error s -> raise (E s) | Ok v -> v)
+           (fun v ->
+             match f v with
+             | Error err ->
+               exit := Some err;
+               raise Exit
+             | Ok v -> v )
            l )
-    with E s -> Error s
+    with Exit -> Error (Stdlib.Option.get !exit)
+
+  let list_filter_map ~f l =
+    let exit = ref None in
+    let exception Exit in
+    try
+      Ok
+        (List.filter_map
+           (fun v ->
+             match f v with
+             | Error err ->
+               exit := Some err;
+               raise Exit
+             | Ok v -> v )
+           l )
+    with Exit -> Error (Stdlib.Option.get !exit)
 end
