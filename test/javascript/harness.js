ed4562e23cc385b972c4e274b7eee7ad9f100a42
andreffnascimento <andreffnascimento@outlook.com>
2024-02-18 18:21:51 +0000

    test(js): organize array tests

--- test/javascript/harness.js
+++ test/javascript/harness.js
@@ -7,3 +7,8 @@
 function AssertEquals(x, y) {
 	return x === y;
 }
+
+function CompareArrays(x, y) {
+	return x.length === y.length &&
+		x.every((element, index) => element === y[index]);
+}
--- test/js/array/array_sort1.js
+++
@@ -1,4 +0,0 @@
-var arr = [undefined, undefined, undefined, 1, undefined, undefined, 2];
-arr.length = 50;
-arr.sort();
-console.log(arr);
