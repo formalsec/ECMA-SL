68e13ef12fa64e516f709b50da9519c9bfc207aa
andreffnascimento <andreffnascimento@outlook.com>
2024-02-18 20:55:34 +0000

    test(esl): remove promise tests

--- test/js/promise/promise1.js
+++
@@ -1,8 +0,0 @@
-var p = new Promise(function (resolve, reject) {
-	resolve(5);
-});
-
-p.then(function (v) {
-	assert.sameValue(v, 5);
-	console.log("I got: " + v);
-});
--- test/js/promise/promise2.js
+++
@@ -1,8 +0,0 @@
-var p = new Promise(function (resolve, reject) {
-	reject(4);
-});
-
-p.then(undefined, function (v) {
-	assert.sameValue(v, 4);
-	console.log("I got: " + v);
-});
