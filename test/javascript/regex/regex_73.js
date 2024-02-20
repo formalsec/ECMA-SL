074572319a4e789224ee2540854798e30eecf3c5
andreffnascimento <andreffnascimento@outlook.com>
2024-02-20 14:05:08 +0000

    test(js): remove inline harness from regex tests

--- test/javascript/regex/regex_73.js
+++ test/javascript/regex/regex_73.js
@@ -1,4 +1,3 @@
-function Assert(cond) { if (cond !== true) { throw new Error("Assertion failed!"); } } function AssertUnreachable() { throw new Error("Assertion failed!"); } function AssertEquals(val, exp) { return Assert(val === exp); } function AssertArray(arr, exp) { return Assert(arr.length === exp.length && arr.every((element, index) => element === exp[index])); } function AssertObject(obj, exp) { return Assert(Object.keys(obj).length === Object.keys(exp).length && Object.keys(obj).every((fld, _) => obj[fld] === exp[fld])) }
 /**
  * * javascript/regex/regex_73.js
  * 
