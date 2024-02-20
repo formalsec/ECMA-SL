96eb7347394e4af59f8c1a944f6ea6bcb38ba14f
andreffnascimento <andreffnascimento@outlook.com>
2024-02-20 12:42:29 +0000

    refactor(bin): change file paths to fpath

--- bin/commands/cmd_execute.ml
+++ bin/commands/cmd_execute.ml
@@ -1,59 +1,65 @@
 open Ecma_sl
 
 type options =
-  { input : string
-  ; harness : string option
-  ; execute_lang : Enums.Lang.t
-  ; execute_version : Enums.ECMARef.t
-  ; interpret_verbose : bool
-  ; interpret_verbose_at : bool
-  ; interpret_debugger : bool
-  ; interpret_show_result : bool
-  ; interpret_hide_prints : bool
+  { input : Fpath.t
+  ; harness : Fpath.t option
+  ; lang : Enums.Lang.t
+  ; ecmaref : Enums.ECMARef.t
+  ; verbose : bool
+  ; verbose_at : bool
+  ; debugger : bool
+  ; show_exitval : bool
+  ; hide_prints : bool
   }
 
 let langs : Enums.Lang.t list = Enums.Lang.[ Auto; JS; CESL ]
 
-let merge_input_harness (opts : options) : string =
-  match opts.harness with
-  | None -> opts.input
+let merge_input_harness (input : Fpath.t) (harness : Fpath.t option) : Fpath.t =
+  match harness with
+  | None -> input
   | Some harness ->
-    let input_code = Io.read_file opts.input in
-    let harness_code = Io.read_file harness in
+    let input_code = Io.read_file (Fpath.to_string input) in
+    let harness_code = Io.read_file (Fpath.to_string harness) in
     let file = "/tmp/ecmasl-ast.js" in
     Io.write_file file (harness_code ^ input_code);
-    file
+    Fpath.v file
 
-let execute ?(exit_checker_f : Val.t -> Val.t = Cmd_interpret.esl_exit_checker)
-  (opts : options) : unit =
+let execute_cesl (resolve_exitval_f : Val.t -> Val.t) (input : Fpath.t)
+  (ecmaref : Enums.ECMARef.t) (show_exitval : bool) : unit =
   let open Ecma_sl in
-  let finterp = Enums.ECMARef.interp opts.execute_version in
+  let finterp = Enums.ECMARef.interp ecmaref in
   let interp = Parsing_utils.load_file finterp in
-  let ast = Parsing_utils.load_file opts.input in
+  let ast = Parsing_utils.load_file (Fpath.to_string input) in
   String.concat ";\n" [ ast; interp ]
   |> Parsing_utils.parse_prog
-  |> Cmd_interpret.run_interpreter "main"
-  |> Cmd_interpret.process_result exit_checker_f opts.interpret_show_result
+  |> Cmd_interpret.interpret "main"
+  |> Cmd_interpret.process_exitval resolve_exitval_f show_exitval
 
-let encode_and_execute
-  ?(exit_checker_f : Val.t -> Val.t = Cmd_interpret.esl_exit_checker)
-  (opts : options) : unit =
-  let input = merge_input_harness opts in
-  let output = "/tmp/ecmasl-ast.cesl" in
-  Cmd_encode.encode None input (Some output);
-  execute ~exit_checker_f { opts with input = output }
+let execute_js (resolve_exitval_f : Val.t -> Val.t) (input : Fpath.t)
+  (harness : Fpath.t option) (ecmaref : Enums.ECMARef.t) (show_exitval : bool) :
+  unit =
+  let input' = merge_input_harness input harness in
+  let output' = Fpath.v "/tmp/ecmasl-ast.cesl" in
+  Cmd_encode.encode None input' (Some output');
+  execute_cesl resolve_exitval_f output' ecmaref show_exitval
 
 let run (opts : options) : unit =
-  let open Enums.Lang in
-  let valid_langs = valid langs opts.execute_lang in
-  match Cmd.test_file_ext valid_langs opts.input with
-  | Some CESL -> execute opts
-  | Some JS | _ -> encode_and_execute opts
+  let valid_langs = Enums.Lang.valid_langs langs opts.lang in
+  match Enums.Lang.resolve_file_lang valid_langs opts.input with
+  | Some JS ->
+    execute_js Cmd_interpret.exitval_checker_esl opts.input opts.harness
+      opts.ecmaref opts.show_exitval
+  | Some CESL ->
+    execute_cesl Cmd_interpret.exitval_checker_esl opts.input opts.ecmaref
+      opts.show_exitval
+  | _ ->
+    execute_js Cmd_interpret.exitval_checker_esl opts.input opts.harness
+      opts.ecmaref opts.show_exitval
 
 let main (copts : Options.Common.t) (opts : options) : int =
   Options.Common.set copts;
-  Config.Interpreter.verbose := opts.interpret_verbose;
-  Config.Interpreter.verbose_at := opts.interpret_verbose_at;
-  Config.Interpreter.debugger := opts.interpret_debugger;
-  Config.Interpreter.hide_prints := opts.interpret_hide_prints;
+  Config.Interpreter.verbose := opts.verbose;
+  Config.Interpreter.verbose_at := opts.verbose_at;
+  Config.Interpreter.debugger := opts.debugger;
+  Config.Interpreter.hide_prints := opts.hide_prints;
   Cmd.eval_cmd (fun () -> run opts)
--- bin/commands/cmd_interpret.ml
+++ bin/commands/cmd_interpret.ml
@@ -1,78 +1,83 @@
 open Ecma_sl
 
 type options =
-  { input : string
-  ; interpret_lang : Enums.Lang.t
-  ; interpret_verbose : bool
-  ; interpret_verbose_at : bool
-  ; interpret_debugger : bool
-  ; interpret_main : string
-  ; interpret_show_result : bool
-  ; interpret_hide_prints : bool
+  { input : Fpath.t
+  ; lang : Enums.Lang.t
+  ; verbose : bool
+  ; verbose_at : bool
+  ; debugger : bool
+  ; main : string
+  ; show_exitval : bool
+  ; hide_prints : bool
   ; untyped : bool
   }
 
 let langs : Enums.Lang.t list = Enums.Lang.[ Auto; ESL; CESL; CESLUnattached ]
 
-let configure_debugger () : (module Debugger.M) =
-  match !Config.Interpreter.debugger with
-  | true -> (module Debugger.Enable : Debugger.M)
-  | false -> (module Debugger.Disable : Debugger.M)
+module InterpreterConfig = struct
+  let debugger () : (module Debugger.M) =
+    match !Config.Interpreter.debugger with
+    | true -> (module Debugger.Enable : Debugger.M)
+    | false -> (module Debugger.Disable : Debugger.M)
 
-let configure_verbose () : (module Verbose.M) =
-  match !Config.Interpreter.verbose with
-  | true -> (module Verbose.Enable : Verbose.M)
-  | false -> (module Verbose.Disable : Verbose.M)
+  let verbose () : (module Verbose.M) =
+    match !Config.Interpreter.verbose with
+    | true -> (module Verbose.Enable : Verbose.M)
+    | false -> (module Verbose.Disable : Verbose.M)
 
-let configure_monitor () : (module Monitor.M) =
-  (module Monitor.Default : Monitor.M)
+  let monitor () : (module Monitor.M) = (module Monitor.Default : Monitor.M)
+end
 
-let run_interpreter (main : string) (prog : Prog.t) : Val.t =
-  let module Debugger = (val configure_debugger ()) in
-  let module Verbose = (val configure_verbose ()) in
-  let module Monitor = (val configure_monitor ()) in
-  let module Interpreter = Interpreter.M (Debugger) (Verbose) (Monitor) in
-  Interpreter.eval_prog ~main prog
+let exitval_checker_none (retval : Val.t) : Val.t = retval
 
-let no_exit_checker (retval : Val.t) : Val.t = retval
-
-let esl_exit_checker (retval : Val.t) : Val.t =
+let exitval_checker_esl (retval : Val.t) : Val.t =
   match retval with
   | Val.Tuple [ Val.Bool false; retval' ] -> retval'
   | Val.Tuple [ Val.Bool true; err ] ->
     Eslerr.(runtime (UncaughtExn (Val.str err)))
   | _ -> Eslerr.runtime (UnexpectedExitFmt retval)
 
-let process_result (check_result_f : Val.t -> Val.t) (show_result : bool)
-  (retval : Val.t) : unit =
-  let retval' = check_result_f retval in
-  if show_result then Log.app "» exit value: %a\n" Ecma_sl.Val.pp retval'
+let interpret (main : string) (prog : Prog.t) : Val.t =
+  let module Debugger = (val InterpreterConfig.debugger ()) in
+  let module Verbose = (val InterpreterConfig.verbose ()) in
+  let module Monitor = (val InterpreterConfig.monitor ()) in
+  let module Interpreter = Interpreter.M (Debugger) (Verbose) (Monitor) in
+  Interpreter.eval_prog ~main prog
 
-let interpret_core (check_result_f : Val.t -> Val.t) (opts : options) : unit =
-  Parsing_utils.load_file opts.input
-  |> Parsing_utils.parse_prog ~file:opts.input
-  |> run_interpreter opts.interpret_main
-  |> process_result check_result_f opts.interpret_show_result
+let process_exitval (resolve_exitval_f : Val.t -> Val.t) (show_exitval : bool)
+  (exitval : Val.t) : unit =
+  let exitval' = resolve_exitval_f exitval in
+  if show_exitval then Log.app "» exit value: %a\n" Ecma_sl.Val.pp exitval'
 
-let compile_and_interpret (opts : options) : unit =
-  Cmd_compile.run_compiler opts.input
-  |> run_interpreter opts.interpret_main
-  |> process_result esl_exit_checker opts.interpret_show_result
+let interpret_cesl (resolve_exitval_f : Val.t -> Val.t) (input : Fpath.t)
+  (main : string) (show_exitval : bool) : unit =
+  let input' = Fpath.to_string input in
+  Parsing_utils.load_file input'
+  |> Parsing_utils.parse_prog ~file:input'
+  |> interpret main
+  |> process_exitval resolve_exitval_f show_exitval
+
+let interpret_esl (input : Fpath.t) (main : string) (show_exitval : bool) : unit
+    =
+  Cmd_compile.compile input
+  |> interpret main
+  |> process_exitval exitval_checker_esl show_exitval
 
 let run (opts : options) : unit =
-  let open Enums.Lang in
-  let valid_langs = valid langs opts.interpret_lang in
-  match Cmd.test_file_ext valid_langs opts.input with
-  | Some ESL -> compile_and_interpret opts
-  | Some CESL -> interpret_core esl_exit_checker opts
-  | Some CESLUnattached -> interpret_core no_exit_checker opts
-  | _ -> compile_and_interpret opts
+  let valid_langs = Enums.Lang.valid_langs langs opts.lang in
+  match Enums.Lang.resolve_file_lang valid_langs opts.input with
+  | Some ESL -> interpret_esl opts.input opts.main opts.show_exitval
+  | Some CESL ->
+    interpret_cesl exitval_checker_esl opts.input opts.main opts.show_exitval
+  | Some CESLUnattached ->
+    interpret_cesl exitval_checker_none opts.input opts.main opts.show_exitval
+  | _ -> interpret_esl opts.input opts.main opts.show_exitval
 
 let main (copts : Options.Common.t) (opts : options) : int =
   Options.Common.set copts;
-  Config.Interpreter.verbose := opts.interpret_verbose;
-  Config.Interpreter.verbose_at := opts.interpret_verbose_at;
-  Config.Interpreter.debugger := opts.interpret_debugger;
-  Config.Interpreter.hide_prints := opts.interpret_hide_prints;
+  Config.Interpreter.verbose := opts.verbose;
+  Config.Interpreter.verbose_at := opts.verbose_at;
+  Config.Interpreter.debugger := opts.debugger;
+  Config.Interpreter.hide_prints := opts.hide_prints;
   Config.Tesl.untyped := opts.untyped;
   Cmd.eval_cmd (fun () -> run opts)
