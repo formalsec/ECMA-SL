1b458a0e8b64b07f48b8ab4dc6942416cd33e68e
andreffnascimento <andreffnascimento@outlook.com>
2024-02-23 10:04:54 +0000

    refactor(bin): moved js2ecma-sl command to independent module

--- bin/commands/cmd_encode.ml
+++ bin/commands/cmd_encode.ml
@@ -1,34 +1,16 @@
+open Ecma_sl
+
 type options =
   { inputs : Fpath.t
   ; output : Fpath.t option
   ; builder : string option
   }
 
-module JS2ECMA_SL = struct
-  open Bos
-
-  let set_output (output : string option) (cmd : Cmd.t) : Cmd.t =
-    match output with
-    | None -> cmd
-    | Some output' -> Cmd.(add_args cmd (v "-o" % output'))
-
-  let set_builder (builder : string option) (cmd : Cmd.t) : Cmd.t =
-    match builder with
-    | None -> cmd
-    | Some builder' -> Cmd.(add_args cmd (v "-b" % builder'))
-
-  let cmd (input : string) (output : string option) (builder : string option) :
-    Cmd.t =
-    Cmd.(v "js2ecma-sl" % "-s" % "-c" % "-i" % input)
-    |> set_output output
-    |> set_builder builder
-end
-
 let encode (builder : string option) (input : Fpath.t) (output : Fpath.t option)
   : unit =
   let input' = Fpath.to_string input in
   let output' = Option.map Fpath.to_string output in
-  match Bos.OS.Cmd.run (JS2ECMA_SL.cmd input' output' builder) with
+  match Bos.OS.Cmd.run (Js2ecmasl.cmd input' output' builder) with
   | Error _ -> raise (Cmd.Command_error Error)
   | Ok _ -> Ecma_sl.Log.debug "File '%s' has been encoded." input'
 
