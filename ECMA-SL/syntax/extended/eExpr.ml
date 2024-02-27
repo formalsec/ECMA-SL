623f4ccb8430af066b18851c66dcf06d6a3343e4
andreffnascimento <andreffnascimento@outlook.com>
2024-02-27 16:49:11 +0000

    refactor(esl): move parsing utils to parsing helper module

--- ECMA-SL/syntax/extended/eExpr.ml
+++ ECMA-SL/syntax/extended/eExpr.ml
@@ -18,16 +18,6 @@ and t' =
   | Curry of t * t list
   | Symbolic of Type.t * t
 
-module Parser = struct
-  let parse_obj_flds (flds : (Id.t * 'a) list) : (Id.t * 'a) list =
-    let check_dups checked (fn, _) =
-      if not (Hashtbl.mem checked fn.it) then Hashtbl.replace checked fn.it ()
-      else Eslerr.(compile ~src:(ErrSrc.at fn) (DuplicatedField fn.it))
-    in
-    List.iter (check_dups (Hashtbl.create (List.length flds))) flds;
-    flds
-end
-
 let rec pp (fmt : Fmt.t) (e : t) : unit =
   let open Fmt in
   match e.it with
