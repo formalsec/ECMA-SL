e8d100552d16467b0411b05c0a52fe225e524e56
andreffnascimento <andreffnascimento@outlook.com>
2024-03-02 14:38:16 +0000

    chore(tesl): clean old files from typed ecma-sl

--- bin/commands/cmd_compile.ml
+++ bin/commands/cmd_compile.ml
@@ -7,13 +7,14 @@ type options =
   }
 
 let type_check (prog : EProg.t) : EProg.t =
-  if !Config.Tesl.untyped || true then prog
-  else
-    let terrs = T_Checker.type_program prog in
-    if terrs = [] then prog
-    else (
-      Fmt.eprintf "%s" (T_Checker.terrs_str terrs);
-      raise (Cmd.Command_error Cmd.Error) )
+  if !Config.Tesl.untyped then prog else prog
+(* Uncomment once the type system is working properly *)
+(* else *)
+(* let terrs = T_Checker.type_program prog in *)
+(* if terrs = [] then prog *)
+(* else ( *)
+(* Fmt.eprintf "%s" (T_Checker.terrs_str terrs); *)
+(* raise (Cmd.Command_error Cmd.Error) ) *)
 
 let compile (input : Fpath.t) : Prog.t =
   let compile' file =
