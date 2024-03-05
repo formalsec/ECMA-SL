dd829e0202de2a59cc55d0568c712ad6e44221c4
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-03-05 17:57:16 +0000

    I can't believe shell paths were valid js programs

--- js2esl/src/js_parser.ml
+++ js2esl/src/js_parser.ml
@@ -1,4 +1,6 @@
 let from_file ~(filename : string) :
   ((Loc.t, Loc.t) Flow_ast.Program.t, Types.exit_value) Result.t =
-  let (ast, errors) = Parser_flow.program filename in
-  match errors with [] -> Ok ast | _ -> Error (`Parser_error errors)
+  In_channel.with_open_text filename (fun chan ->
+      let program_text = In_channel.input_all chan in
+      let (ast, errors) = Parser_flow.program program_text in
+      match errors with [] -> Ok ast | _ -> Error (`Parser_error errors) )
