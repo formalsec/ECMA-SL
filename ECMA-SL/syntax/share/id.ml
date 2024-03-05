e6d24013065b5ac945370f94cfbf33d02b92cd1f
andreffnascimento <andreffnascimento@outlook.com>
2024-03-05 01:14:48 +0000

    address PR #76 review

--- ECMA-SL/syntax/share/id.ml
+++ ECMA-SL/syntax/share/id.ml
@@ -4,5 +4,6 @@ type t = t' Source.phrase
 and t' = string
 
 let default () : t = "" @> no_region
+let equal (id1 : t) (id2 : t) : bool = String.equal id1.it id2.it
 let pp (fmt : Fmt.t) (id : t) : unit = Fmt.fprintf fmt "%s" id.it
 let str (id : t) : string = Fmt.asprintf "%a" pp id
