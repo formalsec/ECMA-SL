fdabf5463b20409bdfd64337a9dd8fa9c6d57140
arckenimuz <andreffnascimento@outlook.com>
2024-04-23 16:32:30 +0100

    refactor(bin): improve the symbolic command

--- bin/commands/cmd_explodejs.ml
+++ bin/commands/cmd_explodejs.ml
@@ -1,8 +1,8 @@
-let run () (opt : Cmd_symbolic.options) =
+let run () (opt : Cmd_symbolic.Options.t) =
   let n = Cmd_symbolic.run () opt in
   (* if n <> 0 then n *)
   if n <> () then ()
   else
-    let filename = opt.filename in
+    let filename = opt.input in
     let testsuite = Fpath.(opt.workspace / "test-suite") in
     Cmd_replay.run () { filename; testsuite }
