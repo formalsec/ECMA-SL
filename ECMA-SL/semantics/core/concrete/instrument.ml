4929bbbd8830cd203ea7cdff8e7ae5af4fc14533
arckenimuz <andreffnascimento@outlook.com>
2024-05-03 15:03:28 +0100

    feat(interpreter): add profiling instrumentation

--- ECMA-SL/semantics/core/concrete/instrument.ml
+++ ECMA-SL/semantics/core/concrete/instrument.ml
@@ -3,10 +3,12 @@ type stmt_eval = Monitor.stmt_eval
 module type M = sig
   module Tracer : Tracer.M
   module Debugger : Debugger.M
+  module Profiler : Profiler.M
   module Monitor : Monitor.M
 
   type t =
     { db : Debugger.t
+    ; pf : Profiler.t
     ; mon_state : Monitor.state
     ; mon_label : Monitor.sl_label
     }
@@ -15,19 +17,26 @@ module type M = sig
   val cleanup : t -> unit
 end
 
-module Default (Tr : Tracer.M) (Db : Debugger.M) (Mon : Monitor.M) : M = struct
+module Default
+    (Tr : Tracer.M)
+    (Db : Debugger.M)
+    (Pf : Profiler.M)
+    (Mon : Monitor.M) : M = struct
   module Tracer = Tr
   module Debugger = Db
+  module Profiler = Pf
   module Monitor = Mon
 
   type t =
     { db : Debugger.t
+    ; pf : Profiler.t
     ; mon_state : Monitor.state
     ; mon_label : Monitor.sl_label
     }
 
   let initial_state () : t =
     { db = Debugger.initial_state ()
+    ; pf = Profiler.initial_state ()
     ; mon_state = Monitor.initial_state ()
     ; mon_label = Monitor.initial_label ()
     }
