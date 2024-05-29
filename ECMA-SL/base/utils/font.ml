abf77ab785e04e0a1ba94736f074bc4ed352a4f3
arckenimuz <andreffnascimento@outlook.com>
2024-05-29 14:46:09 +0100

    chore(base): polish the font module

--- ECMA-SL/base/utils/font.ml
+++ ECMA-SL/base/utils/font.ml
@@ -1,12 +1,13 @@
 module Config = struct
   let colored = ref true
-  let supported_stdout = Terminal.colored Unix.stdout
-  let supported_stderr = Terminal.colored Unix.stderr
 end
 
-type t = t' list
+let colored (fdesc : Unix.file_descr option) (ppf : Fmt.t) : bool =
+  !Config.colored
+  && Option.fold ~none:true ~some:Terminal.colored fdesc
+  && (ppf == Fmt.std_formatter || ppf == Fmt.err_formatter)
 
-and t' =
+type font_v =
   | Normal
   | Bold
   | Faint
@@ -26,23 +27,10 @@ and t' =
   | Cyan
   | White
 
-let colored (fdesc : Unix.file_descr option) (ppf : Fmt.t) : bool =
-  let supported_fdesc fdesc =
-    if fdesc == Unix.stdout then Config.supported_stdout
-    else if fdesc == Unix.stderr then Config.supported_stderr
-    else false
-  in
-  if not !Config.colored then false
-  else
-    Option.fold ~none:true ~some:supported_fdesc fdesc
-    && (ppf == Fmt.std_formatter || ppf == Fmt.err_formatter)
+type t = font_v list
 
-let clean (text : string) : string =
-  let escape_regex = Str.regexp "\027\\[[0-9;]*m" in
-  Str.global_replace escape_regex "" text
-
-let pp_code (ppf : Fmt.t) (font_el : t') : unit =
-  match font_el with
+let pp_font_v (ppf : Fmt.t) (font_v : font_v) : unit =
+  match font_v with
   | Normal -> Fmt.pp_str ppf "0"
   | Bold -> Fmt.pp_str ppf "1"
   | Faint -> Fmt.pp_str ppf "2"
@@ -63,30 +51,23 @@ let pp_code (ppf : Fmt.t) (font_el : t') : unit =
   | White -> Fmt.pp_str ppf "37"
 
 let pp_font (ppf : Fmt.t) (font : t) : unit =
-  Fmt.(format ppf "\027[%am" (pp_lst !>";" pp_code) font)
+  Fmt.(format ppf "\027[%am" (pp_lst !>";" pp_font_v) font)
 
 let pp ?(fdesc : Unix.file_descr option = None) (font : t)
-  (pp_el : Fmt.t -> 'a -> unit) (ppf : Fmt.t) (el : 'a) : unit =
-  if not (colored fdesc ppf) then pp_el ppf el
-  else Fmt.format ppf "%a%a%a" pp_font font pp_el el pp_font [ Normal ]
+  (pp_v : Fmt.t -> 'a -> unit) (ppf : Fmt.t) (v : 'a) : unit =
+  if not (colored fdesc ppf) then pp_v ppf v
+  else Fmt.format ppf "%a%a%a" pp_font font pp_v v pp_font [ Normal ]
 
 let str ?(fdesc : Unix.file_descr option = None) (font : t)
-  (pp_el : Fmt.t -> 'a -> unit) (el : 'a) : string =
-  Fmt.str "%a" (pp ~fdesc font pp_el) el
+  (pp_v : Fmt.t -> 'a -> unit) (v : 'a) : string =
+  Fmt.str "%a" (pp ~fdesc font pp_v) v
 
-let pp_text ?(fdesc : Unix.file_descr option = None) (font : t) (ppf : Fmt.t)
-  (s : string) =
-  pp ~fdesc font Fmt.pp_str ppf s
-
-let str_text ?(fdesc : Unix.file_descr option = None) (font : t) (s : string) =
-  Fmt.str "%a" (pp_text ~fdesc font) s
-
-let pp_none _font pp_el ppf el = pp_el ppf el
-let pp_out font pp_el ppf el = pp ~fdesc:(Some Unix.stdout) font pp_el ppf el
-let pp_err font pp_el ppf el = pp ~fdesc:(Some Unix.stderr) font pp_el ppf el
-let str_out font pp_el el = str ~fdesc:(Some Unix.stdout) font pp_el el
-let str_err font pp_el el = str ~fdesc:(Some Unix.stderr) font pp_el el
-let pp_text_out font ppf s = pp_text ~fdesc:(Some Unix.stdout) font ppf s
-let pp_text_err font ppf s = pp_text ~fdesc:(Some Unix.stderr) font ppf s
-let str_text_out font s = str_text ~fdesc:(Some Unix.stdout) font s
-let str_text_err font s = str_text ~fdesc:(Some Unix.stderr) font s
+let pp_none _font pp_v ppf v = pp_v ppf v
+let pp_out font pp_v ppf v = pp ~fdesc:(Some Unix.stdout) font pp_v ppf v
+let pp_err font pp_v ppf v = pp ~fdesc:(Some Unix.stderr) font pp_v ppf v
+let str_out font pp_v v = str ~fdesc:(Some Unix.stdout) font pp_v v
+let str_err font pp_v v = str ~fdesc:(Some Unix.stderr) font pp_v v
+let pp_text_out font ppf s = pp ~fdesc:(Some Unix.stdout) font Fmt.pp_str ppf s
+let pp_text_err font ppf s = pp ~fdesc:(Some Unix.stderr) font Fmt.pp_str ppf s
+let str_text_out font s = str ~fdesc:(Some Unix.stdout) font Fmt.pp_str s
+let str_text_err font s = str ~fdesc:(Some Unix.stderr) font Fmt.pp_str s
