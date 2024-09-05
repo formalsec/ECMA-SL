b6f5c0e33bc944e2a2c1869be298f42d65a0b23d
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-09-05 13:40:20 +0100

    Fix duplicated newlines in symbolic debug logs

--- ECMA-SL/semantics/core/functorial/solver.ml
+++ ECMA-SL/semantics/core/functorial/solver.ml
@@ -1,1 +1,6 @@
+open EslBase
 include Smtml.Solver.Batch (Smtml.Z3_mappings)
+
+let check solver assumptions =
+  Log.debug "@[<hov 1>      solver:@ %a@]" Smtml.Expr.pp_list assumptions;
+  check solver assumptions
