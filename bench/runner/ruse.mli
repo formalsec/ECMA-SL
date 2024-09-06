2349164ea814ae8bc148d8481bc839395ac9c323
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-09-06 19:06:12 +0100

    Add compositional benchmark runner for symbolic command

---
+++ bench/runner/ruse.mli
@@ -0,0 +1,5 @@
+type t = Rusage.t
+type who = Rusage.who
+
+val get : who -> t
+val sub : t -> t -> t
---
+++ bench/runner/tool.mli
@@ -0,0 +1,4 @@
+type t
+
+val symbolic : unit -> t
+val cmd : t -> workspace:Fpath.t -> file:Fpath.t -> string * string array
