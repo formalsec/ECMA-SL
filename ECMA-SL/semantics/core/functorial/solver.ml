451951b2ebeb85d48c19e8895f30dea55c44b189
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-09-08 00:06:15 +0100

    Improve symbolic execution debug logs

--- ECMA-SL/semantics/core/functorial/solver.ml
+++ ECMA-SL/semantics/core/functorial/solver.ml
@@ -2,5 +2,14 @@ open EslBase
 include Smtml.Solver.Batch (Smtml.Z3_mappings)
 
 let check solver assumptions =
-  Log.debug "@[<hov 1>      solver:@ %a@]" Smtml.Expr.pp_list assumptions;
+  Log.debug_k (fun pp ->
+      pp "@[<hov 1>      solver:@ %a@]" Smtml.Expr.pp_list assumptions );
   check solver assumptions
+
+let pp_set fmt v =
+  let space fmt () = Fmt.fmt fmt "@ " in
+  Smtml.Expr.Set.pretty ~pp_sep:space Smtml.Expr.pp fmt v
+
+let check_set solver assumptions =
+  Log.debug_k (fun pp -> pp "@[<hov 1>      solver:@ %a@]" pp_set assumptions);
+  check_set solver assumptions
