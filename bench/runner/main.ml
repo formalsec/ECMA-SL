79fbe9ae06ed35b319d018a70da0545573564a70
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-09-10 01:33:15 +0100

    Runner: add timeout and output log to file

--- bench/runner/main.ml
+++ bench/runner/main.ml
@@ -1,6 +1,7 @@
 open Bos
 open Smtml.Syntax.Result
 
+let timeout = 30
 let symbolic = Tool.symbolic ()
 let _ = [ symbolic ]
 let dataset_dir = Fpath.v "./buckets.js"
@@ -28,26 +29,47 @@ let run_single workspace file =
     let (prog, args) = Tool.cmd symbolic ~workspace ~file in
     Unix.execvp prog args )
   else
-    let (waited_pid, status) = Unix.waitpid [] pid in
-    let clock = Unix.gettimeofday () -. start_time in
-    let rusage = Ruse.sub (Ruse.get Children) start_rusage in
-    assert (waited_pid = pid);
-    Exec_result.{ clock; file; rusage; status }
+    let _ = Unix.alarm timeout in
+    Sys.set_signal Sys.sigalrm (Signal_handle (fun (_ : int) -> Unix.kill pid 9));
+    Fun.protect
+      ~finally:(fun () -> Sys.set_signal Sys.sigalrm Signal_default)
+      (fun () ->
+        let (waited_pid, status) =
+          try Unix.waitpid [] pid
+          with Unix.Unix_error (Unix.EINTR, _, _) -> Unix.waitpid [] pid
+        in
+        let clock = Unix.gettimeofday () -. start_time in
+        let rusage = Ruse.sub (Ruse.get Children) start_rusage in
+        assert (waited_pid = pid);
+        Exec_result.{ clock; file; rusage; status } )
 
 let result_dir =
   let tm = Unix.localtime @@ Unix.gettimeofday () in
   Fmt.kstr Fpath.v "./results-%04d%02d%02d_%02dh%02dm%02ds" (tm.tm_year + 1900)
     (tm.tm_mon + 1) tm.tm_mday tm.tm_hour tm.tm_min tm.tm_sec
 
+let results_ =
+  let _ = OS.Dir.create result_dir in
+  Fpath.(result_dir / "results.txt")
+
+let oc = Out_channel.open_text @@ Fpath.to_string results_
+
+let pp fmt =
+  Fun.protect ~finally:Out_channel.flush_all (fun () ->
+      Fmt.kstr (Out_channel.output_string oc) fmt )
+
 let results =
   let* dataset in
   (* Evaluated in reverse order *)
+  let i = ref 0 in
+  let n = List.length dataset in
   list_map
     (fun file ->
+      incr i;
       let workspace = Fpath.(result_dir // base file) in
       let+ _ = OS.Dir.create ~path:true workspace in
       let exec_result = run_single workspace file in
-      Fmt.epr "%a@." Exec_result.pp exec_result;
+      pp "Run %d/%d %a@." !i n Exec_result.pp exec_result;
       exec_result )
     dataset
 
@@ -71,5 +93,8 @@ let pp_summary fmt results =
   Fmt.pf fmt "Elapsed time %as" Fmt.float clock_total
 
 let () =
-  let results = ok_or_failwith results in
-  Fmt.pr "%a@." pp_summary results
+  Fun.protect
+    ~finally:(fun () -> Out_channel.close oc)
+    (fun () ->
+      let results = ok_or_failwith results in
+      pp "%a@." pp_summary results )
