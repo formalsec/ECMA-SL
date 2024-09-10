44ee0f578c1ccd3e61cc5114dbd0aa99880a00a4
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-09-10 01:33:15 +0100

    Runner: add timeout and output log to file

--- bench/runner/exec_result.ml
+++ bench/runner/exec_result.ml
@@ -11,5 +11,5 @@ let pp_status fmt = function
   | Unix.WSTOPPED n -> Fmt.pf fmt "Stopped %a" Fmt.int n
 
 let pp fmt { clock; file; rusage; status } =
-  Fmt.pf fmt "@[<v 2>Run %a@;%a in %a %a %a@]" Fpath.pp file pp_status status
+  Fmt.pf fmt "@[<v 2>%a@;%a in %a %a %a@]" Fpath.pp file pp_status status
     Fmt.float clock Fmt.float rusage.utime Fmt.float rusage.stime
