64a258148ede5a3b1dd204f50cc18946490b4cd0
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-12-09 11:47:30 +0000

    Remove testes migrated to ecma-sl

--- test/ecma-sl/symbolic/nodejs/test_sink_fs.js
+++
@@ -1,4 +0,0 @@
-let esl_symbolic = require("esl_symbolic");
-let fs = require("fs");
-let source = esl_symbolic.string("source");
-fs.readFile(source);
--- test/ecma-sl/symbolic/nodejs/test_vfunexported.js
+++
@@ -1,5 +0,0 @@
-let exec = require('child_process').exec;
-
-module.exports = function f(x) {
-  return exec(x);
-};
