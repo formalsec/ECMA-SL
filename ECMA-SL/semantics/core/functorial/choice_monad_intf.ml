a5f03c386ec61307cded87c981a317099993d704
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-12-27 00:24:08 +0000

    Sequencify choice monad (Closes #101)
    Symbolic execution choice monad is now defined using a Seq.t instead of a
    List, essentially behaving as a generator.
    
    Unit tests seem to take considerably longer with this change. More
    benchmarking is required.

--- ECMA-SL/semantics/core/functorial/choice_monad_intf.ml
+++ ECMA-SL/semantics/core/functorial/choice_monad_intf.ml
@@ -1,16 +1,18 @@
 module type Base = sig
   module V : Value_intf.T
 
+  (* TODO: rename to state? *)
   type thread
   type 'a t
 
   val return : 'a -> 'a t
   val stop : 'a t
-  val run : 'a t -> thread -> ('a * thread) list
+  val run : 'a t -> thread -> ('a * thread) Seq.t
   val bind : 'a t -> ('a -> 'b t) -> 'b t
   val ( let* ) : 'a t -> ('a -> 'b t) -> 'b t
   val map : 'a t -> ('a -> 'b) -> 'b t
   val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
+  val with_thread : (thread -> 'a) -> 'a t
 end
 
 module type Complete = sig
