c4193fe643aeb118c4ef60f44fff5b6e51c41fbe
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-01-14 11:57:27 +0000

    Add harness argument to `symbolic` command

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,15 @@
 (* Copyright (C) 2022-2025 formalsec programmers
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *)
@@ -129,8 +129,9 @@ let symbolic_term =
   and+ input = Docs.FileOpts.input
   and+ lang = Docs.SymbolicOpts.lang
   and+ target = Docs.SymbolicOpts.target
-  and+ workspace = Docs.SymbolicOpts.workspace in
-  Cmd_symbolic.run ~input ~lang ~target ~workspace
+  and+ workspace = Docs.SymbolicOpts.workspace
+  and+ harness = Docs.ExecuteOpts.harness in
+  Cmd_symbolic.run ~input ~lang ~target ~workspace ~harness
 
 let symbolic_cmd =
   let open Docs.SymbolicCmd in
