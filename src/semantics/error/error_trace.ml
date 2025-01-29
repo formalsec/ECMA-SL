4826d03383b3a038911fa714c831118a1fb86a7f
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-01-29 12:11:55 +0000

    Fixes additional line break when printing error trace

--- src/semantics/error/error_trace.ml
+++ src/semantics/error/error_trace.ml
@@ -8,14 +8,14 @@ type t = store Call_stack.t
 module RtTraceFmt (ErrorType : Error_type.ERROR_TYPE) = struct
   let pp_frame (ppf : Format.formatter) (frame : store frame) : unit =
     let { f; s; _ } = cursor frame in
-    Fmt.pf ppf "@\nCalled from '%s' in %a" (Func.name' f) pp_loc s.at
+    Fmt.pf ppf "Called from '%s' in %a" (Func.name' f) pp_loc s.at
 
   let pp (ppf : Format.formatter) (stack : t) : unit =
-    let pp_stack = Fmt.(list pp_frame) in
+    let pp_stack = Fmt.(list ~sep:(fun fmt () -> Fmt.pf fmt "@;") pp_frame) in
     match stack with
     | [] -> Log.fail "expecting non-empty call stack"
     | frame :: stack' ->
-      Fmt.pf ppf "@\nRaised at %a%a" pp_frame frame pp_stack stack'
+      Fmt.pf ppf "@;@[<v 4>Raised at %a@;%a@]" pp_frame frame pp_stack stack'
 
   let str (stack : t) : string = Fmt.str "%a" pp stack [@@inline]
 end
