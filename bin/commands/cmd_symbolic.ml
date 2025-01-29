a1a3b88ece960898b531828be023e98f22a975bd
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-01-29 00:56:53 +0000

    Start making the symbolic interpreter functorial

--- bin/commands/cmd_symbolic.ml
+++ bin/commands/cmd_symbolic.ml
@@ -3,6 +3,9 @@ module Choice = Ecma_sl_symbolic.Choice_monad.Seq
 module Thread = Ecma_sl_symbolic.Choice_monad.Thread
 module Solver = Ecma_sl_symbolic.Solver
 
+module Symbolic_interpreter =
+  Ecma_sl_symbolic.Symbolic_interpreter.Make (Ecma_sl_symbolic.Symbolic_error)
+
 let valid_languages : Enums.Lang.t list = Enums.Lang.[ Auto; JS; ESL; CESL ]
 
 let prog_of_js fpath =
@@ -32,7 +35,7 @@ let serialize_thread workspace =
   let open Ecma_sl in
   let mode = 0o666 in
   let (next_int, _) = Base.make_counter 0 1 in
-  fun (result, thread) ->
+  fun thread witness ->
     let open Fpath in
     let pc = Thread.pc thread in
     Logs.debug (fun pp -> pp "@[<hov 1>  path cond :@ %a@]" Solver.pp_set pc);
@@ -40,25 +43,21 @@ let serialize_thread workspace =
     match Solver.check_set solver pc with
     | `Unsat | `Unknown ->
       (* Should not happen. But it does? *)
-      ()
+      [ witness ]
     | `Sat ->
       let model = Solver.model solver in
-      let path =
-        Fmt.kstr (add_seg workspace)
-          (match result with Ok () -> "testcase-%d" | Error _ -> "witness-%d")
-          (next_int ())
-      in
+      let path = Fmt.kstr (add_seg workspace) "witness-%d" (next_int ()) in
       let pp = Fmt.option Smtml.Model.pp in
       let pc = Smtml.Expr.Set.to_list @@ pc in
       let _ = Bos.OS.File.writef ~mode (path + ".js") "%a" pp model in
       let _ =
         Bos.OS.File.writef ~mode (path + ".smtml") "%a" Smtml.Expr.pp_smt pc
       in
-      ()
+      [ witness ]
 
 let write_report workspace symbolic_report =
   let mode = 0o666 in
-  let json = Ecma_sl_symbolic.Symbolic_report.to_json symbolic_report in
+  let json = Symbolic_interpreter.Symbolic_result.to_json symbolic_report in
   let path = Fpath.(workspace / "symbolic-execution.json") in
   Result.bos
   @@ Bos.OS.File.writef ~mode path "%a" (Yojson.pretty_print ~std:true) json
@@ -68,13 +67,13 @@ let run ~input ~lang ~target ~workspace =
   let testsuite = Fpath.(workspace / "test-suite") in
   let* _ = Result.bos (Bos.OS.Dir.create ~mode:0o777 testsuite) in
   let (result, report) =
-    Ecma_sl_symbolic.Symbolic_interpreter.run ~no_stop_at_failure:false ~target
+    Symbolic_interpreter.run ~no_stop_at_failure:false ~target
       ~callback:(serialize_thread testsuite)
       input prog
   in
-  if report.num_failures = 0 then Logs.app (fun k -> k "All Ok!")
-  else Logs.app (fun k -> k "Found %d problems!" report.num_failures);
   Logs.debug (fun k -> k "  exec time : %fs" report.execution_time);
   Logs.debug (fun k -> k "solver time : %fs" report.solver_time);
+  if report.num_failures = 0 then Logs.app (fun k -> k "All Ok!")
+  else Logs.app (fun k -> k "Found %d problems!" report.num_failures);
   let* () = write_report workspace report in
   match result with Ok () -> Ok () | Error err -> Error (`Symbolic err)
--- src/symbolic/symbolic_report.ml
+++ src/symbolic/symbolic_report.ml
@@ -1,25 +1,38 @@
-type t =
-  { filename : Fpath.t
-  ; mutable execution_time : float
-  ; mutable solver_time : float
-  ; mutable solver_queries : int
-  ; mutable num_failures : int
-  ; mutable failures : Symbolic_error.t list
-  }
+module type M = sig
+  type t
 
-let to_json
-  { filename
-  ; execution_time
-  ; solver_time
-  ; solver_queries
-  ; num_failures
-  ; failures
-  } =
-  `Assoc
-    [ ("filename", `String (Fpath.to_string filename))
-    ; ("execution_time", `Float execution_time)
-    ; ("solver_time", `Float solver_time)
-    ; ("solver_queries", `Int solver_queries)
-    ; ("num_failures", `Int num_failures)
-    ; ("failures", `List (List.map Symbolic_error.to_json failures))
-    ]
+  val to_json : t -> Yojson.t
+end
+
+module type S = sig
+  type t
+
+end
+
+module Make (Error : M) = struct
+  type t =
+    { filename : Fpath.t
+    ; mutable execution_time : float
+    ; mutable solver_time : float
+    ; mutable solver_queries : int
+    ; mutable num_failures : int
+    ; mutable failures : Error.t list
+    }
+
+  let to_json
+    { filename
+    ; execution_time
+    ; solver_time
+    ; solver_queries
+    ; num_failures
+    ; failures
+    } =
+    `Assoc
+      [ ("filename", `String (Fpath.to_string filename))
+      ; ("execution_time", `Float execution_time)
+      ; ("solver_time", `Float solver_time)
+      ; ("solver_queries", `Int solver_queries)
+      ; ("num_failures", `Int num_failures)
+      ; ("failures", `List (List.map Error.to_json failures))
+      ]
+end
