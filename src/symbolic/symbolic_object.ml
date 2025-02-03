81a000209b011b3a5602af94d31c12c57590056c
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-02-03 18:27:51 +0000

    Add copyright information to ECMA-SL's interpreter

--- src/symbolic/symbolic_object.ml
+++ src/symbolic/symbolic_object.ml
@@ -1,3 +1,19 @@
+(* Copyright (C) 2022-2025 formalsec programmers
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *)
+
 let eq v1 v2 = Smtml.Expr.relop Ty_bool Eq v1 v2
 let ite c v1 v2 = Smtml.Expr.Bool.ite c v1 v2
 let undef = Symbolic_value.mk_symbol "undefined"
