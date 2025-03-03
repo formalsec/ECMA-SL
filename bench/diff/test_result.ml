c2211f584c1bafd7a4d9105b857679fd653d01d2
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-03-03 19:03:15 +0000

    Add `Skipped` test262 outcome to difftool

--- bench/diff/test_result.ml
+++ bench/diff/test_result.ml
@@ -2,8 +2,10 @@ type t =
   | Success
   | Failure
   | Anomaly
+  | Skipped
 
 let pp fmt = function
   | Success -> Format.pp_print_string fmt "SUCCESS"
   | Failure -> Format.pp_print_string fmt "FAILURE"
   | Anomaly -> Format.pp_print_string fmt "ANOMALY"
+  | Skipped -> Format.pp_print_string fmt "SKIPPED"
